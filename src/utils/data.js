c9fc5b6e053eaa8a2a07122d93e24ef8a1f60384
Elias Yona <iconelias001@gmail.com>
2023-10-22 03:30:22 +0300

    Fix: Some performance tuning

--- src/utils/data.js
+++ src/utils/data.js
@@ -5,29 +5,29 @@ import { flattenData } from "./helpers.js"
 
 export function fetchCustomersData() {
     const fetchData = async () => {
-        let data;
         try {
             const response = await axios.get('http://localhost:8000/customers/');  
-            data = response.data
+            const data = response.data
+
+            const results = data.results
+            const finalData = []
+
+            for (let singleData of results) {
+                const flattenedData = flattenData(singleData)
+                const dataSchema = {
+                    count: data.count,
+                    next: data.next,
+                    previous: data.previous,
+                    ...flattenedData,
+                }
+                finalData.push(dataSchema)
+            }
+
+            return finalData
+
         } catch (error) {
             console.error('Error fetching data:', error); 
-        }
-
-        let results = data.results
-        let dataSchema = {
-            count: data.count,
-            next: data.next,
-            previous: data.previous,
-        }
-        let finalData = []
-
-        for (let singleData of results) {
-            let flattenedData = flattenData(singleData)
-            Object.assign(dataSchema, flattenedData)
-            finalData.push(dataSchema)
-        }
-
-        return finalData
+        } 
     };
 
     return fetchData();
