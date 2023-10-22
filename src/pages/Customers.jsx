a9446fdc2be88f8cbaa63d7cbe56939d631eaa7e
Elias Yona <iconelias001@gmail.com>
2023-10-22 04:08:02 +0300

    feat: Display a spinner while data is still loading

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+import { TailSpin } from  'react-loader-spinner'
 
 import { customersData, customersGrid } from '../data/dummy';
 import { Header } from '../components';
@@ -11,44 +12,56 @@ const Customers = () => {
   const toolbarOptions = ['Delete'];
   const editing = { allowDeleting: true, allowEditing: true };
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // const fetchData = async () => {
-    //   try {
-    //     const response = await axios.get('http://localhost:8000/customers/');  
-    //     setData(response.data);
-    //   } catch (error) {
-    //     console.error('Error fetching data:', error); 
-    //   }
-    // };
+    const fetchData = async () => {
+      try {
+        const customersData = await fetchCustomersData();
+        setData(customersData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-    // fetchData();  
-    setData(fetchCustomersData())
+    fetchData();
   }, []);
-
-  console.log(data)
-  //customersData = flattenData(data.results)
-  
-  console.log(data)
   
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Customers" />
-      <GridComponent
-        dataSource={data}
-        enableHover={false}
-        allowPaging
-        pageSettings={{ pageCount: 1 }}
-        selectionSettings={selectionsettings}
-        toolbar={toolbarOptions}
-        editSettings={editing}
-        allowSorting
-      >
-        <ColumnsDirective>
-          {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-        </ColumnsDirective>
-        <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
-      </GridComponent>
+      {isLoading ? (
+        <div className="flex justify-center">
+          <TailSpin
+            height="50"
+            width="50"
+            color="blue"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <GridComponent
+          dataSource={data}
+          enableHover={false}
+          allowPaging
+          pageSettings={{ pageCount: 1 }}
+          selectionSettings={selectionsettings}
+          toolbar={toolbarOptions}
+          editSettings={editing}
+          allowSorting
+        >
+          <ColumnsDirective>
+            {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+          </ColumnsDirective>
+          <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+        </GridComponent>
+      )}
     </div>
   )
 }
