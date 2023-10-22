b132bd2bdcd480828630ddcf0e95c362d94e851b
Elias Yona <iconelias001@gmail.com>
2023-10-22 08:32:01 +0300

    chore: Some refactoring

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,34 +1,28 @@
 import axios from 'axios'
-
 import { flattenData } from "./helpers.js"
 
+export async function fetchCustomersData() {
+    try {
+        const response = await axios.get('http://localhost:8000/customers');  
+        const data = response.data
 
-export function fetchCustomersData() {
-    const fetchData = async () => {
-        try {
-            const response = await axios.get('http://localhost:8000/customers/');  
-            const data = response.data
+        const results = data.results
+        const finalData = []
 
-            const results = data.results
-            const finalData = []
-
-            for (let singleData of results) {
-                const flattenedData = flattenData(singleData)
-                const dataSchema = {
-                    count: data.count,
-                    next: data.next,
-                    previous: data.previous,
-                    ...flattenedData,
-                }
-                finalData.push(dataSchema)
+        for (let singleData of results) {
+            const flattenedData = flattenData(singleData)
+            const dataSchema = {
+                count: data.count,
+                next: data.next,
+                previous: data.previous,
+                ...flattenedData,
             }
+            finalData.push(dataSchema)
+        }
 
-            return finalData
-
-        } catch (error) {
-            console.error('Error fetching data:', error); 
-        } 
-    };
-
-    return fetchData();
+        return finalData;
+    } catch (error) {
+        console.error('Error fetching data:', error); 
+        throw error; // Re-throw the error to be handled by the caller if needed
+    }
 }
